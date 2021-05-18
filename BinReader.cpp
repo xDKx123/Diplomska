1e37592d98f5acd58b4fb76725f5d0d7eb6b4628
xDKx123 <38501298+xDKx123@users.noreply.github.com>
2021-05-18 12:44:11 +0200

    Implementacija binarnega branja datotek

--- BinReader.cpp
+++ BinReader.cpp
@@ -1,1 +1,41 @@
 #include "BinReader.h"
+
+BinReader::BinReader(std::string path)
+{
+	input.open(path, std::ios::binary);
+	byte = 0;
+	counter = 0;
+}
+
+BinReader::~BinReader()
+{
+	input.close();
+}
+
+bool BinReader::readBit() {
+	if (counter == 8) {
+		input.read((char*)&byte, 1);
+		counter == 0;
+	}
+
+	bool b = (byte >> counter) & 1;
+	counter++;
+	return b;
+}
+
+char BinReader::readByte() {
+	input.read((char*)&byte, 1);
+	return byte;
+}
+
+int BinReader::readInt() {
+	int i;
+	input.read((char*)&i, 4);
+	return i;
+}
+
+float BinReader::readFloat() {
+	float f;
+	input.read((char*)&f, 4);
+	return f;
+}
--- BinReader.h
+++ BinReader.h
@@ -1,5 +1,20 @@
 #pragma once
+#include <fstream>
+#include <string>
 class BinReader
 {
+private:
+	std::ifstream input;
+	char byte;
+	int counter;
+
+public:
+	BinReader(std::string path);
+	~BinReader();
+
+	bool readBit();
+	char readByte();
+	int readInt();
+	float readFloat();
 };
 
